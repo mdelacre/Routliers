90a8a4e134fb044078337f0e03904bfe2eef864d
Marie Delacre <marie.delacre@ulb.ac.be>
2019-03-07 15:37:50 +0100

    Split outliers_mad into 2 functions (one for printing outliers, and one for plotting outliers)

--- R/plot_outliers_mad.R
+++ R/plot_outliers_mad.R
@@ -1,19 +1,19 @@
 #' MAD function to detect outliers
-#' @export outliers_mad
+#' @export plot_outliers_mad
 #' @param data vector of numeric values from which we want to compute outliers
 #' @param constant scale factor
 #' @param threshold the number of MAD considered as a threshold to consider a value an outlier
 #' @param na.rm set whether Missing Values should be excluded (na.rm=TRUE) or not (na.rm=FALSE) - defaults to TRUE
 #' @param verbose logical variable indicating whether text output should be generated (verbose = TRUE) or not (verbose = FALSE) - default to TRUE
-#' @keywords MAD outliers
-#' @return Returns median, MAD, lower MAD limit, upper MAD limit, extremely small values, extremely high values
+#' @keywords plot MAD outliers
 #' @examples
 #' Run outliers_mad
-#' outliers_mad(data=rnorm(150), constant=1.4826,threshold=3,na.rm=TRUE,
+#' plot_outliers_mad(data=rnorm(150), constant=1.4826,threshold=3,na.rm=TRUE,
 #' plot=TRUE, verbose=TRUE)
 #' @importFrom stats na.omit
+#' @importFrom graphics legend par points rect segments text title
 
-outliers_mad=function(data,
+plot_outliers_mad=function(data,
                       constant = 1.4826,
                       threshold=3,
                       na.rm = TRUE,
@@ -37,26 +37,22 @@ outliers_mad=function(data,
   # calculate the outliers
   outliers=c(dat[dat<LL_CI_MAD],dat[dat>UL_CI_MAD])
   outliers_pos=c(which(dat<LL_CI_MAD),which(dat>UL_CI_MAD))
+
   # plotting results
+    plot(NA, ylim=c(min(min(dat),LL_CI_MAD)-1.5*abs(min(min(dat),LL_CI_MAD)),max(max(dat),UL_CI_MAD)+1.5*abs(max(max(dat),UL_CI_MAD))),xlim=c(0,1), bty="n",xaxt="n", ylab="",xlab="")
+    rect(.25, LL_CI_MAD, .35, UL_CI_MAD, col = "lightgrey", border = "lightgrey", lwd = par("lwd"))
+    segments(0.25,LL_CI_MAD,0.35,LL_CI_MAD, lwd=1)
+    text(.35,LL_CI_MAD,"lower MAD limit",lwd=1,pos=4,cex=.75)
+    segments(0.25,UL_CI_MAD,0.35,UL_CI_MAD, lwd=1)
+    text(.35,UL_CI_MAD,"upper MAD limit",lwd=1,pos=4,cex=.75)
+    segments(0.25,center,0.35,center, lwd=3,col="red")
+    text(.35,center,"median",lwd=1,pos=4,cex=.75)
+    if (length(outliers)!=0){
+        points(rep(.3,length(outliers_pos)),dat[outliers_pos],col="red",bg="red",pch=19,cex=.5)
+        text(.3, outliers, paste("POS",as.character(outliers_pos)),pos=4,cex=.75,col="red")}
 
-  if(verbose == TRUE){
-    cat("Results:\n")
-    cat("Median:",format(center, digits = 1, nsmall = 3, scientific = FALSE))
-    cat("\n")
-    cat("MAD:",format(MAD, digits = 1, nsmall = 3, scientific = FALSE))
-    cat("\n\n")
-    cat("Acceptable range of values:")
-    cat("\n")
-    cat("lower MAD limit:", paste0(round(LL_CI_MAD, digits = 4)),"\nupper MAD limit:",paste0(round(UL_CI_MAD, digits = 4)))
-    cat("\n\n")
-    cat("Outliers:")
-    cat("\n\n")
-    cat("extremely small values:", "\npositions:",which(dat<LL_CI_MAD),"\nvalues:",paste0(round(dat[dat<LL_CI_MAD], digits = 4)))
-    cat("\n\n")
-    cat("extremely high values:", "\npositions:",which(dat>UL_CI_MAD),"\nvalues:",paste0(round(dat[dat>UL_CI_MAD], digits = 4)))
-  }
-
-  # Return results in list()
-  invisible(list(Median = center, MAD = MAD, LL_CI_MAD = LL_CI_MAD,UL_CI_MAD = UL_CI_MAD,L_outliers=dat[dat<LL_CI_MAD],U_outliers=dat[dat>UL_CI_MAD],outliers=c(dat[dat<LL_CI_MAD],dat[dat>UL_CI_MAD])))
+        title(main=paste("Detecting outliers \n Threshold=",threshold,"MAD around the median"))
 
 }
+
+
