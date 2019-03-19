d0b3011178f81fa5df6a2fd862c7db974dcd9302
Marie Delacre <marie.delacre@ulb.ac.be>
2019-03-19 13:30:13 +0100

    Improve the print method

--- R/outliers_mad.R
+++ R/outliers_mad.R
@@ -2,7 +2,7 @@
 #'
 #' Detecting univariate outliers using the robust median absolute deviation
 #'
-#' @param data vector of values from which we want to compute outliers
+#' @param x vector of values from which we want to compute outliers
 #' @param b constant depending on the assumed distribution underlying the data, that equals 1/Q(0.75).  When the normal distribution is assumed, the constant 1.4826 is used (and it makes the MAD and SD of normal distributions comparable).
 #' @param threshold the number of MAD considered as a threshold to consider a value an outlier
 #' @param na.rm set whether Missing Values should be excluded (na.rm=TRUE) or not (na.rm=FALSE) - defaults to TRUE
@@ -13,29 +13,31 @@
 #' @return Returns median, MAD, lower MAD limit, upper MAD limit, extremely small values, extremely high values
 #' @examples
 #' ## Run outliers_mad
-#' outliers_mad(data=runif(150,-100,100), b=1.4826,threshold=3,na.rm=TRUE)
-#' outliers_mad(data=Intention$age)
-#' outliers_mad(data=Intention$Total_Amount_Earned)
+#'
+#' outliers_mad(x=runif(150,-100,100), b=1.4826,threshold=3,na.rm=TRUE)
+#' res=outliers_mad(x=Intention$age)
+#' outliers_mad(x=Intention$Total_Amount_Earned)
 #' SOC=rowMeans(Attacks[,c("soc1r","soc2r","soc3r","soc4","soc5","soc6","soc7r","soc8","soc9","soc10r","soc11","soc12","soc13")])
-#' res=outliers_mad(data=SOC)
+#' outliers_mad(x=SOC)
 #'
 #' @importFrom stats na.omit
 
-outliers_mad=function(data,
+# Create a function that compute all required parameters
+outliers_madEst=function(x,
                       b = 1.4826,
                       threshold=3,
                       na.rm = TRUE){
 
   # If data are numeric or integer, applying the function. Otherwise, stopping it.
-  if(inherits(data,c("numeric","integer"))==FALSE) stop("Data are neither numeric nor integer")
+  if(inherits(x,c("numeric","integer"))==FALSE) stop("Data are neither numeric nor integer")
 
   if (na.rm==TRUE) {
-    dat=na.omit(data)   # incomplete cases are removed
-  } else {dat=data}
+    data=na.omit(x)   # incomplete cases are removed
+  } else {data=x}
 
   # Calculate the MAD
-  center=median(dat)
-  MAD=b*median(abs(dat-center))
+  center=median(data)
+  MAD=b*median(abs(data-center))
   half_CI=threshold*MAD # how many MAD from the median are the limits of the IC?
 
   # Calculate the range of acceptable values
@@ -43,30 +45,49 @@ outliers_mad=function(data,
   UL_CI_MAD=center+half_CI # upper limit of the median CI
 
   # calculate the outliers
-  outliers=c(dat[dat<LL_CI_MAD],dat[dat>UL_CI_MAD])
-  outliers_pos=c(which(dat<LL_CI_MAD),which(dat>UL_CI_MAD))
-
-    print.outliers_MAD <- function(x){
-    cat("Results:\n")
-    cat("Median:",format(center, digits = 1, nsmall = 3, scientific = FALSE))
-    cat("\n")
-    cat("MAD:",format(MAD, digits = 1, nsmall = 3, scientific = FALSE))
-    cat("\n\n")
-    cat("Acceptable range of values:")
-    cat("\n")
-    cat("lower MAD limit:", paste0(round(LL_CI_MAD, digits = 4)),"\nupper MAD limit:",paste0(round(UL_CI_MAD, digits = 4)))
-    cat("\n\n")
-    cat("Outliers:")
-    cat("\n\n")
-    cat("extremely small values:", "\npositions:",which(dat<LL_CI_MAD),"\nvalues:",paste0(round(dat[dat<LL_CI_MAD], digits = 4)))
-    cat("\n\n")
-    cat("extremely high values:", "\npositions:",which(dat>UL_CI_MAD),"\nvalues:",paste0(round(dat[dat>UL_CI_MAD], digits = 4)))
-  }
+  outliers=c(data[data<LL_CI_MAD],data[data>UL_CI_MAD])
+  outliers_pos=c(which(data<LL_CI_MAD),which(data>UL_CI_MAD))
 
   # Return results in list()
-   invisible(list(Median = center, MAD = MAD, LL_CI_MAD = LL_CI_MAD,UL_CI_MAD = UL_CI_MAD,L_outliers=dat[dat<LL_CI_MAD],U_outliers=dat[dat>UL_CI_MAD],outliers=c(dat[dat<LL_CI_MAD],dat[dat>UL_CI_MAD])))
+   invisible(list(Median = center, MAD = MAD, LL_CI_MAD = LL_CI_MAD,UL_CI_MAD = UL_CI_MAD,L_outliers=data[data<LL_CI_MAD],U_outliers=data[data>UL_CI_MAD],outliers=c(data[data<LL_CI_MAD],data[data>UL_CI_MAD])))
 
 }
 
-outliers_mad(Attacks$age)$L_outliers
+# res=outliers_madEst(x=Intention$age)
+
+# Create a generic function
+outliers_mad <- function(x,...) UseMethod ("outliers_mad")
+
+# Adding a default method in defining a function called outliers_mad.default
+
+outliers_mad.default = function(x,b = 1.4826,threshold=3,na.rm = TRUE){
+  out = outliers_madEst(x,b,threshold,na.rm)
+  out$median=out$Median
+  out$MAD=out$MAD
+  out$call = match.call()
+  out$limits = c(lower=out$LL_CI_MAD,upper=out$UL_CI_MAD)
+  out$nb = c("extremely low"=length(out$L_outliers),"extremely high"=length(out$U_outliers),total=length(out$outliers))
+
+  class(out)="outliers_mad"
+  out
+  }
+
+print.outliers_mad <- function(x){
+  cat("Call:\n")
+  print(x$call)
+
+  cat("\nMedian:\n")
+  print(x$median)
+
+  cat("\nMAD:\n")
+  print(x$MAD)
+
+  cat("\nLimits of acceptable range of values:\n")
+  print(x$limits)
+
+  cat("\nNumber of detected outliers\n")
+  print(x$nb)
+}
+
+
 
