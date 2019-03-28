32240b2939b73fa698e4b99b464d92fb2eeb451c
Marie Delacre <marie.delacre@ulb.ac.be>
2019-03-28 15:40:09 +0100

    create a plot function to plot the output of the "outliers_*" functions

--- R/outliers_mad.R
+++ R/outliers_mad.R
@@ -34,7 +34,7 @@
 #' data(Attacks)
 #' SOC <- rowMeans(Attacks[,c("soc1r","soc2r","soc3r","soc4","soc5","soc6",
 #' "soc7r","soc8","soc9","soc10r","soc11","soc12","soc13")])
-#' outliers_mad(x = SOC)
+#' res=outliers_mad(x = SOC)
 #'
 #' @importFrom stats na.omit median
 
@@ -74,14 +74,18 @@ outliers_madEst <- function(x,
   outliers_pos <- c(which(data < LL_CI_MAD),which(data > UL_CI_MAD))
 
   # Return results in list()
-   invisible(list(Median = center,
+   invisible(list(Min=Min,
+                  Max=Max,
+                  Median = center,
                   MAD = MAD,
                   LL_CI_MAD = LL_CI_MAD,
                   UL_CI_MAD = UL_CI_MAD,
                   L_outliers = sort(data[data < LL_CI_MAD]),
                   U_outliers = sort(data[data > UL_CI_MAD]),
                   outliers = sort(c(data[data < LL_CI_MAD],
-                                    data[data > UL_CI_MAD]))))
+                                    data[data > UL_CI_MAD])),
+                  outliers_pos=outliers_pos,
+                  threshold=threshold))
 
 }
 
@@ -125,7 +129,68 @@ print.outliers_mad <- function(x,...){
   print(x$nb)
 }
 
+plot.outliers_mad <- function(res,x){
+  # plotting results
+  par(mar = c(5.1,3.1,5.1,1.1))
 
+  if(inherits(x,c("numeric","integer")) == FALSE)
+    stop("x is neither numeric nor integer")
+
+  if (na.rm == TRUE) {
+    data <- na.omit(x)   # incomplete cases are removed
+  } else {data <- x}
+
+  plot(NA,
+       xlim = c(min(min(data),res$LL_CI_MAD)-.1*(max(data)-min(data)),
+                max(max(data),res$UL_CI_MAD)+.1*(max(data)-min(data))),
+       ylim = c(0,1),
+       bty = "n",
+       yaxt = "n",
+       ylab = "",
+       xlab = ""
+  )
+
+  rect(res$LL_CI_MAD,.25,
+       res$UL_CI_MAD,.45,
+       col = "lightgrey",
+       border = "lightgrey",
+       lwd = par("lwd"))
+
+  if(res$LL_CI_MAD != res$UL_CI_MAD){
+    segments(res$LL_CI_MAD,0.25,res$LL_CI_MAD,0.45, lwd = 1)
+    text(res$LL_CI_MAD,.45,"lower CI limit",lwd = 1,pos = 3,cex = .75)
+    segments(res$UL_CI_MAD,0.25,res$UL_CI_MAD,0.45, lwd = 1)
+    text(res$UL_CI_MAD,.45,"upper CI limit",lwd = 1,pos = 3,cex = .75)
+    segments(res$median,0.25,res$median,0.45, lwd = 3,col = "red")
+    text(res$median,.45,"median",lwd = 1,pos = 3,cex = .75,col = "red")
+  } else if (res$LL_CI_MAD == res$UL_CI_MAD){
+    segments(res$LL_CI_MAD,0.25,res$LL_CI_MAD,0.45, lwd = 1)
+    text(res$LL_CI_MAD,
+         .45,
+         paste0("lower = upper","\n","CI limit"),
+         lwd = 1,
+         pos = 3,
+         cex = .75)
+  }
+  if (length(res$outliers) != 0){
+    points(data[res$outliers_pos],
+           rep(.35,length(res$outliers_pos)),
+           col = "red",bg = "red",
+           pch = 19,cex = .5)}
+
+  title(
+    main = paste(
+      "Detecting values out of the Confidence Interval \n CI = Median",
+      "\u00B1",res$threshold," MAD"
+    )
+  )
+
+  if(length(data[res$outliers_pos]) == 0){comment <- "No outliers are detected"
+  } else {comment <- paste(length(res$outliers),"outliers are detected")}
+
+  legend("top",comment,pch = 1,col = "white",cex = 1,bty = "n")
+
+}
 
 
 
