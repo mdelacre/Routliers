9e96cbc348eb525d3a926c2fda922d543c5fd556
Marie Delacre <marie.delacre@ulb.ac.be>
2019-03-29 07:22:56 +0100

    adding a pos_display argument + new examples

--- R/plot_outliers_mad.R
+++ R/plot_outliers_mad.R
@@ -1,18 +1,34 @@
 #' Plotting function for the mad
 #'
-#' plotting data and highlighting univariate outliers detected with the MAD function
+#' plotting data and highlighting univariate outliers detected with the outliers_mad function
 #'
 #' @param res vector of numeric values from which we want to compute outliers
 #' @param x data from which the outliers_mad function was performed
+#' @param pos_display set whether the position of outliers in the dataset should be displayed
+#' on the graph (pos_display = TRUE) or not (posdisplay = FALSE)
 #'
 #' @export plot_outliers_mad
 #' @keywords plot MAD outliers
 #' @return None
 #'
+#' @examples
+#'
+#' #### Run outliers_mad and perform plot_outliers_mad on the result
+#' data(Intention)
+#' res=outliers_mad(Intention$age)
+#' plot_outliers_mad(res,x=Intention$age)
+#'
+#'### when the number of outliers is small, one can display the outliers position in the dataset
+#'x=c(rnorm(10),3)
+#'res2=outliers_mad(x)
+#'plot_outliers_mad(res2,x,pos_display=TRUE)
 #' @importFrom stats na.omit
 #' @importFrom graphics par points rect segments text title plot
+#'
+#'
 
-plot_outliers_mad <- function(res,x){
+
+plot_outliers_mad <- function(res,x,pos_display=FALSE){
   # plotting results
   par(mar = c(5.1,3.1,5.1,1.1))
 
@@ -58,6 +74,13 @@ plot_outliers_mad <- function(res,x){
            rep(.35,length(res$outliers_pos)),
            col = "red",bg = "red",
            pch = 19,cex = .5)}
+
+  if (pos_display==TRUE){
+
+    text(data[res$outliers_pos],.35,
+         as.character(res$outliers_pos),
+         col="red",lwd = 1,pos = 3,cex = .75)
+    }
 
   title(
     main = paste(
